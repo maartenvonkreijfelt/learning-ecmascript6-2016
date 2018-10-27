520c2bcd6a4a1ce0926f279e48bd0088c2d6f4f4
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-10-27 16:36:01 +0200

    update

--- start/src/index.js
+++ start/src/index.js
@@ -1,8 +1,7 @@
-for(i= 0; i<45; i++){
+for(let i= 0; i<45; i++) {
     var div = document.createElement('div');
-    div.onclick = function() {
+    div.onclick = function () {
         alert("you clicked on a box #" + i);
     };
-
     document.getElementsByTagName('section')[0].appendChild(div);
 }
