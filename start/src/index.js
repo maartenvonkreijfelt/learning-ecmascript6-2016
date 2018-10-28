05155c5fca7d270369e80f3d7a0a19e48060ec40
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-10-28 12:23:46 +0100

    update

--- start/src/index.js
+++ start/src/index.js
@@ -1,4 +1,11 @@
-constant.birthYear = 1985;
-var birthYear = 1987;
-var age = 2018 - birthyear;
-console.log(age);
+function coldEnough(deg) {
+    const freezingTemp = 32;
+    if (freezingTemp <= deg) {
+        return deg + " is above freezing.";
+    } else {
+        return deg + " is below freezing.";
+    }
+}
+console.log(coldEnough(40));
+console.log(coldEnough(12));
+console.log(coldEnough(-10));
