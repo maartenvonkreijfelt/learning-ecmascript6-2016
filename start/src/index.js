2b5b4ade4c12972d28add25cbd791a9dbca9b888
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-10-30 14:27:42 +0100

    update

--- start/src/index.js
+++ start/src/index.js
@@ -5,3 +5,14 @@ course.set('jest', {description:'testing'});
 console.log(course);
 console.log(course.react);
 console.log(course.get('react'));
+
+var details = new Map([
+    [new Date(), 'today'],
+    ['items', [1,2]]
+]);
+
+console.log(details.size);
+
+details.forEach(function(item){
+    console.log(item)
+});
