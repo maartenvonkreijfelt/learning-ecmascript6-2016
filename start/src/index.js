1f21d503edc1f8b17cfb0df7da835853271a54a1
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-10-31 10:33:26 +0100

    update

--- start/src/index.js
+++ start/src/index.js
@@ -1,15 +1,28 @@
-var books = new Set();
-books.add('Pride and Prejudice');
-books.add('War and Peace')
-    .add('Oliver Twist');
+for (let letter of 'JavaScript') {
+    console.log(letter);
+}
 
-console.log(books);
-console.log('how many books?', books.size);
-console.log('has Oliver Twist?', books.has('Oliver Twist'));
-books.delete('Oliver Twist');
-console.log('has Oliver Twist still?', books.has('Oliver Twist'));
+var topics = ['JavaScript', 'Node', 'React'];
 
-var data = [4,2,4,4,2,5,1,6,7,5,6,8,2,7];
-var set = new Set(data);
-console.log('data.length', data.length);
-console.log('set.size', set.size);
+for (let topic of topics) {
+    console.log(topic);
+}
+
+var topics = new Map();
+topics.set('HTML', '/class/html');
+topics.set('CSS', '/class/css');
+topics.set('JavaScript', '/class/javascript');
+topics.set('Node', '/class/node');
+
+for (let topic of topics.keys()) {
+    console.log(topic, "is the course name");
+}
+
+for (let topic of topics.values()) {
+    console.log("The course description can be found at", topic);
+}
+
+for (let course of topics.entries()) {
+    console.log(course);
+}
+
