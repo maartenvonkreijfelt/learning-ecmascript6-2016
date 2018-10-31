9f800b994c63c5e82f9a53c73256fda7cf03da46
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-10-31 09:59:39 +0100

    update

--- start/src/index.js
+++ start/src/index.js
@@ -1,18 +1,15 @@
-var course = new Map();
-course.set('react', {description: 'ui'});
-course.set('jest', {description:'testing'});
+var books = new Set();
+books.add('Pride and Prejudice');
+books.add('War and Peace')
+    .add('Oliver Twist');
 
-console.log(course);
-console.log(course.react);
-console.log(course.get('react'));
+console.log(books);
+console.log('how many books?', books.size);
+console.log('has Oliver Twist?', books.has('Oliver Twist'));
+books.delete('Oliver Twist');
+console.log('has Oliver Twist still?', books.has('Oliver Twist'));
 
-var details = new Map([
-    [new Date(), 'today'],
-    ['items', [1,2]]
-]);
-
-console.log(details.size);
-
-details.forEach(function(item){
-    console.log(item)
-});
+var data = [4,2,4,4,2,5,1,6,7,5,6,8,2,7];
+var set = new Set(data);
+console.log('data.length', data.length);
+console.log('set.size', set.size);
