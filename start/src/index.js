b5a367af2848d4774ca52fc00de7933252e416c9
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-10-31 12:07:31 +0100

    update

--- start/src/index.js
+++ start/src/index.js
@@ -1,16 +1,43 @@
-function add(x,y){
-console.log(x+y)
-}
-add();
+
+var cat = {
+    meow: function(times) {
+        console.log(Array(times).join("meow"));
+    },
+    purr: function(times) {
+        console.log(Array(times).join("purr"));
+    },
+    snore: function(times) {
+        console.log(Array(times).join("ZZZ"));
+    }
+};
+cat.meow(3);
+cat.purr(4);
+cat.snore(5);
 
 
-function add2(x=5,y=7){
-    console.log(x+y)
-}
-add2();
 
 
-function add3(x=5,y=7){
-    console.log(x+y)
-}
-add3(20,60);
+
+
+
+
+
+
+
+
+
+var cat2 = {
+    meow(times) {
+        console.log("meow".repeat(times));
+    },
+    purr(times) {
+        console.log("purr".repeat(times));
+    },
+    snore(times) {
+        console.log("ZZZ".repeat(times));
+    }
+};
+
+cat2.meow(3);
+cat2.purr(4);
+cat2.snore(5);
