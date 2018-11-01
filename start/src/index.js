ed905401bd6676e3577343fda2ab5a958992d6e9
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-11-01 11:39:12 +0100

    update

--- start/src/index.js
+++ start/src/index.js
@@ -2,8 +2,9 @@ var person = {
     first: "Doug",
     actions: ['bike', 'hike', 'ski', 'surf'],
     printActions: function() {
+        var _this=this;
         this.actions.forEach(function(action) {
-            var str = this.first + " likes to " + action;
+            var str = _this.first + " likes to " + action;
         console.log(str);
     });
     }
