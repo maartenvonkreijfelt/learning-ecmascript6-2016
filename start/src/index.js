1e2e4eedaa5b30c212bc8a789d0653fd2e66bd12
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-11-05 11:53:33 +0100

    update

--- start/src/index.js
+++ start/src/index.js
@@ -1,4 +1,4 @@
-/*
+
 var vacation = {
     destination: "Chile",
     travelers: 2,
@@ -6,21 +6,8 @@ var vacation = {
     cost: 4000
 };
 
-console.log(vacation.destination);
-console.log(vacation.travelers);
-*/
+function vacationMarketing({destination, activity}) {
+    return `Come to ${destination} and do some ${activity}`
+}
 
-
-
-
-
-
-var {destination,travelers} = {
-    destination: "Chile",
-    travelers: 2,
-    activity: "skiing",
-    cost: 4000
-};
-
-console.log(destination);
-console.log(travelers);
+console.log(vacationMarketing(vacation));
