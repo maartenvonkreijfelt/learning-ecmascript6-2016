73f400f090db6f4aec3008466fe2d3b93261b69f
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-11-08 11:50:54 +0100

    update

--- start/src/index.js
+++ start/src/index.js
@@ -1,9 +1,33 @@
-const id = Symbol();
-const courseInfo = {
-    title: "ES6",
-    topics: ["babel", "syntax", "functions", "classes"],
-    id: "js-course"
-};
+var title = 'ES6';
+console.log(typeof title);
 
-courseInfo[id] = 41284;
-console.log(courseInfo);
+console.log(typeof title[Symbol.iterator]);
+
+var iterateIt = title[Symbol.iterator]();
+console.log(iterateIt.next());
+console.log(iterateIt.next());
+console.log(iterateIt.next());
+console.log(iterateIt.next());
+
+
+function tableReady(arr) {
+    var nextIndex = 0;
+    return {
+        next() {
+            if(nextIndex < arr.length) {
+                return {value: arr.shift(), done: false}
+            } else {
+                return {done: true}
+            }
+        }
+    }
+}
+
+var waitingList = ['Sarah', 'Heather', 'Anna', 'Meagan'];
+var iterateList = tableReady(waitingList);
+
+console.log(`${iterateList.next().value}, your table is ready`);
+console.log(`${iterateList.next().value}, your table is ready`);
+console.log(`${iterateList.next().value}, your table is ready`);
+console.log(`${iterateList.next().value}, your table is ready`);
+console.log(`Is this finished? ${iterateList.next().done}`);
